09e9fdce093100e9035932218af978a4c4b0f931
Pras <Pras@gmail.com>
2019-09-09 15:50:34 +0530

    User credentials updated

---
+++ access.py
@@ -0,0 +1,7 @@
+name=str(input("Enter your name"))
+age=int(input("Enter your age"))
+if(name=="Prasanth" and age==25):
+    print("Your name is Prasanth")
+    print("Your age is 25")
+else:
+    print("Access Denied")
